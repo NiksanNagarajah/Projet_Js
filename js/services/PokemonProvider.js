4b6c29ed0a0d0fb2965dfc55bb39f8653b459479
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2025-03-13 09:10:34 +0100

    Pokedex

--- js/services/PokemonProvider.js
+++ js/services/PokemonProvider.js
@@ -33,4 +33,21 @@ export default class PokemonProvider {
             console.log("Error getting pokemon", err);
         }
     }
+
+    static getAllPokemon = async () => {
+        const options = {
+            method : 'GET', 
+            headers : {
+                'Content-Type':'application/json'
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}`, options);
+            // Ajouter un if response.ok ?
+            const json = await response.json(); 
+            return json;
+        } catch(err) {
+            console.log("Error getting documents", err);
+        }
+    }
 } 
